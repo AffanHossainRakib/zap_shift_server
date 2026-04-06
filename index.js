45f6dd69ab4b7571104560989fe1d52a1180318c
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-07 01:11:35 +0600

    Refactor MongoDB connection and parcel API endpoints in index.js

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
+// MongoDB connection
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("zap_shift_db");
+    const parcelsCollection = db.collection("parcels");
+
+    //   Parcel API
+    app.get("/parcels", async (req, res) => {
+      const parcels = await parcelsCollection.find().toArray();
+      res.send(parcels);
+    });
+
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelsCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Zap shift shifting here.");
+  res.send("Zap shift shifting fine.");
 });
 
 app.listen(port, () => {
