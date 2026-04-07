89013195622f5de01f0e168ba3e942325bb59791
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-07 16:52:49 +0600

    feat: add endpoint to get single parcel by ID

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
       res.send(result);
     });
 
+    // Get single parcel by ID
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
