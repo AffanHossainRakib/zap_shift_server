3a089acc8c1bbf4d4712463d07463ff2e6a1c811
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-12 01:12:59 +0600

    feat: add payment collection initialization to MongoDB setup

--- src/config/db.js
+++ src/config/db.js
@@ -16,20 +16,52 @@ const client = new MongoClient(uri, {
 
 let dbConnected = false;
 let parcelsCollection;
+let paymentCollection;
+let initPromise = null;
 
-const getParcelsCollection = async () => {
-  if (dbConnected && parcelsCollection) {
-    return parcelsCollection;
+const initCollections = async () => {
+  if (dbConnected && parcelsCollection && paymentCollection) {
+    return;
   }
 
-  await client.connect();
-  const db = client.db("zap_shift_db");
-  parcelsCollection = db.collection("parcels");
-  dbConnected = true;
+  if (initPromise) {
+    await initPromise;
+    return;
+  }
+
+  initPromise = (async () => {
+    await client.connect();
+    const db = client.db("zap_shift_db");
+    parcelsCollection = db.collection("parcels");
+    paymentCollection = db.collection("payment");
+    dbConnected = true;
+  })();
+
+  try {
+    await initPromise;
+  } catch (error) {
+    dbConnected = false;
+    parcelsCollection = undefined;
+    paymentCollection = undefined;
+    throw error;
+  } finally {
+    initPromise = null;
+  }
+};
+
+const getParcelsCollection = async () => {
+  await initCollections();
 
   return parcelsCollection;
 };
 
+const getPaymentCollection = async () => {
+  await initCollections();
+
+  return paymentCollection;
+};
+
 module.exports = {
   getParcelsCollection,
+  getPaymentCollection,
 };
